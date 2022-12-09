04732936037f1c7aa35dc5300c59000b3a1f93dd
Will <wlymes@gmail.com>
2022-12-09 10:39:12 -0300

    style: header

--- src/components/Layout/Header/index.jsx
+++ src/components/Layout/Header/index.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
-import  * as S from './style'
+import * as S from './style'
 
 export const Header = () => {
-  return (
-    <S.Header>
-        <S.Logo>LOGO</S.Logo>
-        <S.NavBar>
-            <S.List>
-                <S.ItemList>Trilhas</S.ItemList>
-                <S.ItemList>Trilhas</S.ItemList>
-                <S.ItemList>Trilhas</S.ItemList>
-                <S.ItemList>Trilhas</S.ItemList>
-            </S.List>
-        </S.NavBar>
-    </S.Header>
-  )
+    return (
+        <S.Header>
+            <S.Logo>
+                <S.NormalSpan>Will</S.NormalSpan>
+                <S.ColorSpan>Code</S.ColorSpan>
+            </S.Logo>
+            <S.NavBar>
+                <S.List>
+                    <S.ItemList><S.Link>Quem sou eu</S.Link></S.ItemList>
+                    <S.ItemList><S.Link>Projetos</S.Link></S.ItemList>
+                    <S.ItemList><S.Link>Skills & SoftSkills</S.Link></S.ItemList>
+                    <S.ItemList><S.Link>Contato</S.Link></S.ItemList>
+                </S.List>
+            </S.NavBar>
+        </S.Header>
+    )
 }
--- src/components/Layout/Header/style.jsx
+++ src/components/Layout/Header/style.jsx
@@ -1,18 +1,41 @@
 import styled from 'styled-components'
 
+const Palet = styled.div`
+    color:#191919; // fundo
+    color:#101010; //fundo 2
+    color:#f61b12; //gradient
+    color:#f9004d; //font dest
+    color:#ef0a5e; //font 
+    color:#ffffff; //font
+    color:#929d8d; //description
+`
+
 export const Header = styled.header`
     display: flex;
     align-items: center;
     justify-content: space-between;
     padding: 0 4rem;
     height: 5rem;
-    background: #777;
-    border-bottom: 1px solid #555;
+    background: #191919;
+    border-bottom: 1px solid #929d8d;
+
+    @media (max-width: 45rem) {
+        display: none;
+  }
 `
 export const Logo = styled.div`
+    display: flex;
+    font-size: 2rem;
+    font-weight: bold;
+`
+export const NormalSpan = styled.span`
     display: block;
-    color: #eee;
-    background: #333;
+    color: #ffffff;
+`
+
+export const ColorSpan = styled.span`
+    display: block;
+    color: #f9004d;
 `
 export const NavBar = styled.nav`
     display: flex;
@@ -26,9 +49,19 @@ export const List = styled.ul`
 `
 
 export const ItemList = styled.li`
-    color: #eee;
     padding: .4rem;
     margin: .4rem;
-    font-weight: bold;
+`
+
+export const Link = styled.a`
     cursor: pointer;
+    color: #ffffff;
+    font-size: 1.3rem;
+    font-weight: bold;
+    transition: .2s;
+    border: none;
+
+    &:hover{
+        color: #f9004d;
+    }
 `
