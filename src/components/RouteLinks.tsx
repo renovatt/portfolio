0b9f5bf6257ccfe14e4f00138a041bdf4035b02d
will <wlymes@gmail.com>
2023-07-22 10:22:32 -0300

    feat: tagging links by pathname

--- src/components/RouteLinks.tsx
+++ src/components/RouteLinks.tsx
@@ -1,14 +1,18 @@
 import Link from 'next/link';
 import { RouteLinksProps } from '@/@types';
+import { usePathname } from 'next/navigation';
 
 export const RouteLinks = (props: RouteLinksProps) => {
+    const pathname = usePathname()
+    const isActive = pathname === props.href;
+    
     return (
         <Link
-            className={`${props.onClick ? 'text-3xl' : 'text-xl hover:text-textPrimary'} text-white font-bold transition-all ease-in`}
+            className={`${props.onClick ? 'text-3xl' : `text-xl hover:text-textPrimary ${isActive ? 'text-textPrimary' : 'text-white'}`} font-bold transition-all ease-in`}
             href={props.href}
             onClick={props.onClick}
         >
             {props.text}
         </Link>
-    )
-}
+    );
+};
