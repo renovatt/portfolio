60b8483bfa7e2c1fa4d24f86efa31856124087d0
will <wlymes@gmail.com>
2023-08-07 14:27:19 -0300

    feat: dynamic metadata

--- src/app/views/project/[id]/page.tsx
+++ src/app/views/project/[id]/page.tsx
@@ -1,15 +1,53 @@
 import { getProjects, getProjectsById } from "@/services"
 import { ProjectDetails } from "@/app/components/ProjectDetails";
-import { ProjectResponse, ProjectsResponse, ProjectsTypeProps } from "@/@types";
+import { ProjectResponse, ProjectsResponse } from "@/@types";
 
 export async function generateStaticParams() {
     const projects = await getProjects();
     const projectsData = projects as ProjectsResponse;
 
-    return projectsData.projects.map((project: ProjectsTypeProps) => ({
+    return projectsData.projects.map((project) => ({
         id: project.id.toString()
     }));
 };
+
+export async function generateMetadata({ params }: { params: { id: string } }) {
+    const projects = await getProjectsById(params.id);
+    const projectsData = projects as ProjectResponse;
+
+    const APP_NAME = `WillCode | ${projectsData.project_name}`
+    const APP_DESCRIPTION = projectsData.description;
+    const DEPLOY_URL = projectsData.deploy_url;
+    const BANNER_IMAGE = projectsData.banner_url;
+
+    return {
+        title: APP_NAME,
+        applicationName: APP_NAME,
+        description: APP_DESCRIPTION,
+        generator: 'Next.js',
+        manifest: "/manifest.json",
+        keywords: ["renovatt", "Wildemberg", "Wildemberg Renovato", "WillCode"],
+        themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
+        authors: [{ name: 'renovatt' }, { name: 'renovatt', url: 'https://www.linkedin.com/in/renovatt/' }],
+        viewport: "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+
+        openGraph: {
+            type: "website",
+            url: DEPLOY_URL,
+            title: APP_NAME,
+            description: APP_DESCRIPTION,
+            siteName: APP_NAME,
+            images: [{
+                url: BANNER_IMAGE,
+            }],
+        },
+
+        icons: [
+            { rel: "apple-touch-icon", url: "/icon-192x192.png" },
+            { rel: "icon", url: "/favicon.ico" },
+        ],
+    }
+}
 
 export default async function Project({ params }: { params: { id: string } }) {
     const project = await getProjectsById(params.id);
