a6a9246f4d3a590d38ec603f322c4d25930bc8b9
will <wlymes@gmail.com>
2023-09-01 11:51:20 -0300

    refactor: home updated

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -1,28 +1,42 @@
 'use client'
 
 import React from 'react'
+import Link from 'next/link'
 import logo from '@/assets/logo.png'
+import { BsFileEarmarkTextFill } from 'react-icons/bs'
+import { FaLinkedin, FaGithub } from 'react-icons/fa'
 import SkeletonImageProfile from './helpers/SkeletonImageProfile';
+import { useMessage } from '@/hooks/useMessage'
 
 const Home = () => {
-  const [index, setIndex] = React.useState(0);
-  const [textEffect, setText] = React.useState('');
-
-  React.useEffect(() => {
-    const interval = setInterval(() => {
-      setText(prevText => prevText + "Front-end Developer".charAt(index));
-      setIndex(prevIndex => prevIndex + 1);
-    }, 100);
-
-    return () => clearInterval(interval);
-  }, [index]);
+  const cv = '/willcode-cv.pdf'
+  const { textEffect, final } = useMessage()
 
   return (
     <section className='flex items-center justify-center flex-col-reverse md:justify-around md:flex-row w-[95vw] max-w-[1400px] animate-fade transition-all'>
-      <article className='flex flex-col md:items-start items-center w-1/2 mt-4 md:mt-0'>
+      <article className='flex flex-col md:items-start items-center w-1/2 mt-4 md:mt-0 relative'>
         <span className='text-white text-5xl'>Olá, eu sou</span>
         <h1 className='text-white text-8xl font-bold md:text-9xl md:ml-16'>Will<span className='text-textPrimary'>.</span></h1>
         <span className='mt-2 text-textPrimary text-xl md:text-5xl after:content-["|"] after:ml-2 after:animate-blink after:text-white'>{textEffect}</span>
+
+        {final && (
+          <span className='flex items-center justify-center mt-4 animate-fade transition-all absolute -bottom-11
+           md:left-0'>
+            <Link
+              className='flex items-center text-white'
+              href="https://www.linkedin.com/in/renovatt" target="_blank">
+              <FaLinkedin className='h-8 w-8 text-white hover:text-textPrimary transition-all ease-in m-1 hover:-mt-1' />
+            </Link><Link
+              className='flex items-center text-white'
+              href="https://github.com/renovatt" target="_blank">
+              <FaGithub className='h-8 w-8 text-white hover:text-textPrimary transition-all ease-in m-1 hover:-mt-1' />
+            </Link><Link
+              className='flex items-center text-white'
+              href={cv} target="_blank">
+              <BsFileEarmarkTextFill className='h-8 w-8 text-white hover:text-textPrimary transition-all ease-in m-1 hover:-mt-1' />
+            </Link>
+          </span>
+        )}
       </article>
 
       <figure className='w-40 md:w-80'>
