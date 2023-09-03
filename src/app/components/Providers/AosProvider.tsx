7a65863b210c2b75db74cd5856098d6bde5252ae
will <wlymes@gmail.com>
2023-09-03 12:33:39 -0300

    feat: aos provider created

---
+++ src/app/components/Providers/AosProvider.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import { Fragment, useEffect } from 'react'
+
+export type AOSProviderProps = {
+  children: React.ReactNode
+}
+
+export const AosProvider = ({ children }: AOSProviderProps) => {
+  useEffect(() => {
+    AOS.init()
+  }, [])
+
+  return <Fragment>{children}</Fragment>
+}
