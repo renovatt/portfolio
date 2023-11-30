bc3cb5a63ee85b4f2da921ec8ef4ffda47836b87
will <wlymes@gmail.com>
2023-11-30 13:06:30 -0300

    refactor: logo refactored

--- src/components/modules/Header.tsx
+++ src/components/modules/Header.tsx
@@ -1,21 +1,14 @@
 'use client'
-import Link from 'next/link'
 import Navbar from './Navbar'
 import NavbarMobile from './NavbarMobile'
 import { useToggle } from '@hooks/useToggle'
+import Logo from '@elements/Logo'
 
 const Header = () => {
   const { modal, openModal, closeModal } = useToggle()
   return (
-    <header className="container flex h-20 w-full items-center justify-between border-b border-primary-950 bg-primary-900 transition-all">
-      <Link href="/" className="flex text-2xl font-bold md:text-3xl">
-        <h1 className="inline-block text-primary-750">
-          <span>{'{ '}</span>
-          <span className="text-primary-950">...</span>
-          Will
-          <span>{' }'}</span>
-        </h1>
-      </Link>
+    <header className="container flex h-20 w-full items-center justify-between overflow-hidden border-b border-primary-950 bg-primary-900 transition-all">
+      <Logo />
       <Navbar openModal={openModal} />
       <NavbarMobile modal={modal} closeModal={closeModal} />
     </header>
