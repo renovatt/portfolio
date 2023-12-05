faef25bd6e8898a8222f8d5b71cfa57f4dd6ab6f
will <wlymes@gmail.com>
2023-12-04 22:18:37 -0300

    feat: links updated

--- src/constants/Links.ts
+++ src/constants/Links.ts
@@ -1,22 +1,48 @@
-export const Links = [
+import { FaLaptopCode } from 'react-icons/fa6'
+import { MdOutlineEmail } from 'react-icons/md'
+import { HiHome, HiMiniUser } from 'react-icons/hi2'
+import { BsFileEarmarkTextFill } from 'react-icons/bs'
+import { FaGithub, FaLinkedin, FaProjectDiagram } from 'react-icons/fa'
+
+export const PathLinks = [
   {
     href: '/home',
-    text: '<Página inicial />',
+    text: 'Início',
+    icon: HiHome,
   },
   {
     href: '/profile',
-    text: '<Quem sou eu />',
+    text: 'Perfil',
+    icon: HiMiniUser,
   },
   {
     href: '/projects',
-    text: 'Projetos />',
+    text: 'Projetos',
+    icon: FaProjectDiagram,
   },
   {
     href: '/skills',
-    text: '<Habilidades />',
+    text: 'Habilidades',
+    icon: FaLaptopCode,
   },
   {
     href: '/contact',
-    text: '<Contato />',
+    text: 'Contato',
+    icon: MdOutlineEmail,
+  },
+]
+
+export const SocialLinks = [
+  {
+    href: 'https://www.linkedin.com/in/renovatt',
+    icon: FaLinkedin,
+  },
+  {
+    href: 'https://github.com/renovatt',
+    icon: FaGithub,
+  },
+  {
+    href: '/willcode-cv.pdf',
+    icon: BsFileEarmarkTextFill,
   },
 ]
