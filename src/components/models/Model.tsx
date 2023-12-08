11a68990f2bb03c6b3c4e35229275c2a7c78361c
will <wlymes@gmail.com>
2023-12-08 14:28:38 -0300

    refactor: mecha removed

--- src/components/models/Model.tsx
+++ src/components/models/Model.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import { MechBust } from './MechBust'
 import CanvaContainer from './CanvaContainer'
 
 export default function Model() {
   return (
     <CanvaContainer>
-      <MechBust />
+      <h1></h1>
+      {/* <MechBust /> */}
     </CanvaContainer>
   )
 }
