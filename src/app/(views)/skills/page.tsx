48690ed2582ed9027a510b0b059afb00547096b8
will <wlymes@gmail.com>
2023-12-14 14:48:10 -0300

    feat: few styles details

--- src/app/(views)/skills/page.tsx
+++ src/app/(views)/skills/page.tsx
@@ -12,10 +12,10 @@ export const metadata: Metadata = {
 
 export default function Skills() {
   return (
-    <section className="relative flex h-screen w-screen flex-col justify-center overflow-hidden py-20 transition-all">
+    <section className="relative flex h-screen w-screen flex-col justify-center overflow-hidden py-20 transition-all lg:p-0">
       <Ray />
       <BackgroundImage image="bg-skills animate-fade" />
-      <section className="md:pm-0 z-10 my-2 flex h-full w-full flex-col items-center justify-start gap-10 space-y-10 overflow-y-auto overflow-x-hidden px-4 pb-20 md:space-y-0 lg:flex-row lg:justify-around">
+      <section className="z-10 my-2 flex h-full w-full flex-col items-center justify-start gap-10 space-y-10 overflow-y-auto overflow-x-hidden px-4 pb-20 md:space-y-0 md:pb-0 lg:flex-row lg:justify-around">
         <SideSkills />
         <SkillsIcons />
       </section>
--- src/components/elements/Icons.tsx
+++ src/components/elements/Icons.tsx
@@ -9,13 +9,15 @@ export default function Icons({ id, svg_link: svgLink }: SkillsTypeProps) {
   return (
     <motion.section
       drag
+      dragElastic={1}
+      dragConstraints={{ bottom: 0, left: 0, top: 0, right: 0 }}
       variants={fadeIn({ direction: 'up', delay: 0.3 })}
       initial="hidden"
       animate="visible"
       exit="hidden"
       className="item flex h-24 w-24 cursor-grab items-center justify-center"
     >
-      <motion.figure drag className="pointer-events-none -z-10 h-24 w-24">
+      <motion.figure className="pointer-events-none -z-10 h-24 w-24">
         <motion.img
           className="h-full w-full cursor-pointer object-cover"
           src={svgLink}
